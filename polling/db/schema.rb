caa99a2fed2d9d7445772bb684d040c3b4c27810
Karan Kalra <karank@tyemill.com>
2017-07-11 10:29:13 -0700

    add options model

--- polling/db/schema.rb
+++ polling/db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170711155907) do
+ActiveRecord::Schema.define(version: 20170711165822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "options", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "count"
+    t.integer  "poll_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "options", ["poll_id"], name: "index_options_on_poll_id", using: :btree
 
   create_table "polls", force: :cascade do |t|
     t.string   "title"
@@ -23,4 +33,5 @@ ActiveRecord::Schema.define(version: 20170711155907) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "options", "polls"
 end
